5c28da243c3211ef32f72f0437bffd0812d88097
chandrabiji <chandramca04@gmail.com>
2022-10-12 14:22:13 +0530

    first commit

---
+++ src/main/java/com/chandra/JenkinPipeline/JenkinPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.chandra.JenkinPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chandra/JenkinPipeline/JenkinPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandra.JenkinPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
